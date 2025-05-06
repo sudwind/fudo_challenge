f5190762046eecba35c4c4dd26e8258a334546f8
Pablo Luna <git@pabloluna.de>
2025-05-06 09:59:20 -0300

    Improve JWT handling

--- app/infrastructure/services/jwt_service.rb
+++ app/infrastructure/services/jwt_service.rb
@@ -1,25 +1,31 @@
 # typed: true
 require 'jwt'
+require 'securerandom'
 
 module Infrastructure
   module Services
     class JwtService
-      SECRET_KEY = ENV['JWT_SECRET_KEY'] || 'your-secret-key'
       ALGORITHM = 'HS256'
       EXPIRATION_TIME = 24 * 60 * 60 # 24 hours in seconds
 
-      def self.encode(user_id)
-        payload = {
-          user_id: user_id,
-          exp: Time.now.to_i + EXPIRATION_TIME
-        }
-        JWT.encode(payload, SECRET_KEY, ALGORITHM)
-      end
+      class << self
+        def secret_key
+          @secret_key ||= ENV['JWT_SECRET_KEY'] || SecureRandom.hex(32)
+        end
 
-      def self.decode(token)
-        JWT.decode(token, SECRET_KEY, true, { algorithm: ALGORITHM })
-      rescue JWT::DecodeError
-        nil
+        def encode(user_id)
+          payload = {
+            user_id: user_id,
+            exp: Time.now.to_i + EXPIRATION_TIME
+          }
+          JWT.encode(payload, secret_key, ALGORITHM)
+        end
+
+        def decode(token)
+          JWT.decode(token, secret_key, true, { algorithm: ALGORITHM })
+        rescue JWT::DecodeError
+          nil
+        end
       end
     end
   end
